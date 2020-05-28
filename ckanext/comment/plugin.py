ef303fe7b2e05253a767729a5cc1eb5c0b539bf2
glujan <glujan@idom.com>
2020-05-28 02:51:53 -0500

    version 1 - comentarios

---
+++ ckanext/comment/plugin.py
@@ -0,0 +1,185 @@
+import logging
+import base64
+import hashlib
+import hmac
+import simplejson
+import time
+
+from ckan.common import request
+from ckan.lib.helpers import url_for_static_or_external
+import ckan.plugins as p
+
+lang_translations = {
+    'de': 'de',
+    'es': 'es_ES',
+    'sv': 'sv_SE',
+    'pt': 'pt_EU',
+    'sr': 'sr_CYRL',
+    'sr_Latn': 'sr_LATIN',
+    'no': 'en',  # broken no translation available
+}
+
+# all other codes are two letter language code
+
+# Portuguese (Brazil) = pt_BR
+# Portuguese (European) = pt_EU
+# Serbian (Cyrillic) = sr_CYRL
+# Serbian (Latin) = sr_LATIN
+# Spanish (Argentina) = es_AR
+# Spanish (Mexico) = es_MX
+# Spanish (Spain) = es_ES
+# Swedish = sv_SE
+
+#log = logging.getLogger(__name__)
+
+
+class CommentPlugin(p.SingletonPlugin):
+    '''
+    Insert javascript fragments into package pages and the home page to allow
+    users to view and create comments on any package.
+    '''
+    p.implements(p.IConfigurable)
+    p.implements(p.IConfigurer)
+    p.implements(p.ITemplateHelpers)
+
+    def configure(self, config):
+    ##    '''
+    ##    Called upon CKAN setup, will pass current configuration dict to the
+    ##    plugin to read custom options.  To implement Disqus Single Sign On,
+    ##    you must have your secret and public key in the ckan config file. For
+    ##    more info on Disqus SSO see:
+    ##    https://help.disqus.com/customer/portal/articles/236206-integrating-single-sign-on
+    ##    '''
+        ceh_name = config.get('ceh_comment.name', None)
+    ##    disqus_secret_key = config.get('disqus.secret_key', None)
+    ##    disqus_public_key = config.get('disqus.public_key', None)
+        ceh_url = config.get('ceh_comment.url', None)
+        site_url = config.get('ckan.site_url', None)
+        site_title = config.get('ckan.site_title', None)
+        if ceh_name is None:
+            log.warn("No forum name is set. Please set \
+                'ceh_comment.name' in your .ini!")
+    ##    config['pylons.app_globals'].has_commenting = True
+
+    ##    disqus_developer = p.toolkit.asbool(config.get('disqus.developer',
+    ##                                                   'false'))
+    ##    disqus_developer = str(disqus_developer).lower()
+    ##    # store these so available to class methods
+    ##    self.__class__.disqus_developer = disqus_developer
+        self.__class__.ceh_name = ceh_name
+    ##    self.__class__.disqus_secret_key = disqus_secret_key
+    ##    self.__class__.disqus_public_key = disqus_public_key
+        self.__class__.ceh_url = ceh_url
+        self.__class__.site_url = site_url
+        self.__class__.site_title = site_title
+
+    def update_config(self, config):
+        # add template directory to template path
+        p.toolkit.add_template_directory(config, 'templates')
+		p.toolkit.add_public_directory(config_, 'public/ceh_comment')
+        p.toolkit.add_resource('public/ceh_comment', 'res')
+    
+	@classmethod
+    def language(cls):
+        lang = p.toolkit.request.environ.get('CKAN_LANG')
+        if lang in lang_translations:
+            lang = lang_translations[lang]
+        else:
+            lang = lang[:2]
+        return lang
+
+    @classmethod
+    def ceh_comments(cls):
+        '''Add Comments to the page.'''
+
+        c = p.toolkit.c
+
+        # Get user info to send for Disqus SSO
+
+        # Set up blank values
+        message = 'blank'
+        sig = 'blank'
+        timestamp = 'blank'
+
+        # Get the user if they are logged in.
+        user_dict = {}
+        try:
+            user_dict = p.toolkit.get_action('user_show')({'keep_email': True},
+                                                          {'id': c.user})
+
+        # Fill in blanks for the user if they are not logged in.
+        except:
+            user_dict['id'] = ''
+            user_dict['name'] = ''
+            user_dict['email'] = ''
+
+        # Create the SSOm data.
+        SSOdata = simplejson.dumps({
+            'id': user_dict['id'],
+            'username':  user_dict['name'],
+            'email': user_dict['email'],
+            })
+
+        message = base64.b64encode(SSOdata)
+        # generate a timestamp for signing the message
+        timestamp = int(time.time())
+        # generate our hmac signature
+        sig = ''
+        ##if cls.disqus_secret_key is not None:
+        ##    sig = hmac.HMAC(cls.disqus_secret_key, '%s %s' %
+        ##                    (message, timestamp), hashlib.sha1).hexdigest()
+
+        # we need to create an identifier
+        try:
+            identifier = c.controller
+            if identifier == 'package':
+                identifier = 'dataset'
+            if c.current_package_id:
+                identifier += '::' + c.current_package_id
+            elif c.id:
+                identifier += '::' + c.id
+            else:
+                # cannot make an identifier
+                identifier = ''
+            # special case
+            if c.action == 'resource_read':
+                identifier = 'dataset-resource::' + c.resource_id
+        except:
+            identifier = ''
+        data = {'identifier': identifier,
+                ##'developer': cls.disqus_developer,
+                'language': cls.language(),
+                'ceh_shortname': cls.ceh_name,
+
+                # start Koebrick change
+                'site_url': cls.site_url,
+                'site_title': cls.site_title,
+                'message': message,
+                'sig': sig,
+                'timestamp': timestamp}
+                ##'pub_key': cls.disqus_public_key}
+
+        return p.toolkit.render_snippet('ceh_comments.html', data)
+
+    @classmethod
+    def ceh_recent(cls, num_comments=5):
+        '''Add recent comments to the page. '''
+        data = {'ceh_shortname': cls.ceh_name,
+                'ceh_num_comments': num_comments}
+        return p.toolkit.render_snippet('ceh_recent.html', data)
+
+    @classmethod
+    def current_ceh_url(cls, ):
+        '''If `ceh_comment.url` is defined, return a fully qualified url for
+        the current page with `ceh_comment.url` as the base url,'''
+
+        if cls.ceh_url is None:
+            return None
+
+        return url_for_static_or_external(request.environ['CKAN_CURRENT_URL'],
+                                          qualified=True, host=cls.ceh_url)
+
+    def get_helpers(self):
+        return {'ceh_comments': self.ceh_comments,
+                'ceh_recent': self.ceh_recent,
+                'current_ceh_url': self.current_ceh_url}
---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1.0'
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+	name='ceh-comment',
+	version=version,
+	description="plugin de comentarios para CKAN",
+	long_description=long_description,
+	classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+	keywords='ckan, cehcomments, comments, commenting, discussion',
+	author='Gary Luján',
+	author_email='glujan@idom.com',
+	url='https://github.com/glujan04/ckanext-ceh-comment',
+	license='AGPL',
+	packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+	namespace_packages=['ckanext'],
+	include_package_data=True,
+	zip_safe=False,
+	install_requires=[
+		# -*- Extra requirements: -*-
+	],
+	entry_points=\
+	"""
+    [ckan.plugins]
+	ceh_comment = ckanext.comment.plugin:CommentPlugin
+	""",
+)
