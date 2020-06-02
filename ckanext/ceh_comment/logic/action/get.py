dd883a68e971a385caca7e34bfb7d460d7e2b13f
glujan <glujan@idom.com>
2020-06-02 12:31:45 -0500

    v 22

--- ckanext/ceh_comment/logic/action/get.py
+++ ckanext/ceh_comment/logic/action/get.py
@@ -61,11 +61,11 @@ def thread_show(context, data_dict):
 
     if context.get('with_deleted') is not True:
         thread_dict['comments'] = [
-            c.as_dict() for c in comments.order_by('comment.creation_date asc').all()
+            c.as_dict() for c in comments.order_by('ceh_comment.creation_date asc').all()
         ]
     else:
         thread_dict['comments'] = [
-            c.as_dict(only_active_children=False) for c in comments.order_by('comment.creation_date asc').all()
+            c.as_dict(only_active_children=False) for c in comments.order_by('ceh_comment.creation_date asc').all()
         ]
 
     return thread_dict
