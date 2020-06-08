94751ad34ad0783f8f86f38bd21d0820d46edaf8
glujan <glujan@idom.com>
2020-06-08 00:14:24 -0500

    v 22

--- ckanext/ceh_comment/fanstatic/ceh.js
+++ ckanext/ceh_comment/fanstatic/ceh.js
@@ -208,3 +208,15 @@ function addParam(form, name, value) {
     form.appendChild($input);
 }
 
+$('#delModal').on('show.bs.modal', function(e) {
+    //get data-id attribute of the clicked element
+    var threadId = $(e.relatedTarget).data('thread-id');
+    //populate the textbox
+    $(e.currentTarget).find('input[name="tid"]').val(threadId);
+});
+
+function delNotify(){
+	let threadId = $('#tid').val();
+	console.log('threadId',threadId);
+}
+
