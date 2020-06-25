663ee2f90651b5648cdd5944631b3cff51de7fbd
glujan <glujan@idom.com>
2020-06-25 02:06:33 -0500

    24-06-2020

--- ckanext/ceh_comment/controller.py
+++ ckanext/ceh_comment/controller.py
@@ -79,7 +79,7 @@ class CommentController(BaseController):
 
         #return render("package/read.html")
         #return jsonify({'data': render_template('package/read.html')}
-        return render_template("package/read.html")
+        return render_template("package/ceh_comment_list.html", pkg_id=c.pkg.id, pkg_name=c.pkg.name, userobj=c.userobj)
 
     def reply(self, dataset_id, parent_id):
         c.action = 'reply'
