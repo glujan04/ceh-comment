f422caf1b4e01ce211c37bcb9e3e1a315a5c09aa
glujan <glujan@idom.com>
2020-06-25 11:21:54 -0500

    24-06-2020

--- ckanext/ceh_comment/process.py
+++ ckanext/ceh_comment/process.py
@@ -1,18 +1,21 @@
 
-from flask import jsonify,Flask
+from flask import jsonify, Flask, render_template, request
 
 app = Flask(__name__)
 
-    @app.route('/publish2', methods=['POST'])
-    def publish2():
+@app.route('/publish2', methods=['POST'])
+def publish2():
 
-        context = {'model': model, 'user': c.user}
-        #print request.form
+	context = {'model': model, 'user': c.user}
+	#print request.form
 
-        #h.redirect_to(str('/dataset/%s' % c.pkg.name))
+	#h.redirect_to(str('/dataset/%s' % c.pkg.name))
 
-        #return render("package/read.html")
-        #return jsonify({'data': render_template('package/read.html')}
-        #return render_template("package/ceh_comment_list.html", pkg_id=c.pkg.id, pkg_name=c.pkg.name, userobj=c.userobj)
-        data = ['Element 1', ' Element 2', 'Element 3']
-        return jsonify({'result': True, 'data': False})
+	#return render("package/read.html")
+	#return jsonify({'data': render_template('package/read.html')}
+	#return render_template("package/ceh_comment_list.html", pkg_id=c.pkg.id, pkg_name=c.pkg.name, userobj=c.userobj)
+	data = ['Element 1', ' Element 2', 'Element 3']
+	return jsonify({'result': True, 'data': False})
+
+if __name__ == '__main__':
+    app.run(debug=True)
