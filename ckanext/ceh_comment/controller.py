584188d15560620985b2009c6a4305e169ecc783
glujan <glujan@idom.com>
2020-06-25 11:18:54 -0500

    24-06-2020

--- ckanext/ceh_comment/controller.py
+++ ckanext/ceh_comment/controller.py
@@ -80,20 +80,6 @@ class CommentController(BaseController):
 
         return render("package/read.html")
 
-    @app.route('/publish2', methods=['POST'])
-    def publish2():
-
-        context = {'model': model, 'user': c.user}
-        #print request.form
-
-        #h.redirect_to(str('/dataset/%s' % c.pkg.name))
-
-        #return render("package/read.html")
-        #return jsonify({'data': render_template('package/read.html')}
-        #return render_template("package/ceh_comment_list.html", pkg_id=c.pkg.id, pkg_name=c.pkg.name, userobj=c.userobj)
-        data = ['Element 1', ' Element 2', 'Element 3']
-        return jsonify({'result': True, 'data': False})
-
     def reply(self, dataset_id, parent_id):
         c.action = 'reply'
 
---
+++ ckanext/ceh_comment/process.py
@@ -0,0 +1,18 @@
+
+from flask import jsonify,Flask
+
+app = Flask(__name__)
+
+    @app.route('/publish2', methods=['POST'])
+    def publish2():
+
+        context = {'model': model, 'user': c.user}
+        #print request.form
+
+        #h.redirect_to(str('/dataset/%s' % c.pkg.name))
+
+        #return render("package/read.html")
+        #return jsonify({'data': render_template('package/read.html')}
+        #return render_template("package/ceh_comment_list.html", pkg_id=c.pkg.id, pkg_name=c.pkg.name, userobj=c.userobj)
+        data = ['Element 1', ' Element 2', 'Element 3']
+        return jsonify({'result': True, 'data': False})
